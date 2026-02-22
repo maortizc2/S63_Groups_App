e9b2a8490efbb6ac3f788164a5f75f29d6e7bc4e
Santiago Sabogal Lozano <ssabogall@eafit.edu.co>
2026-02-21 20:26:24 -0500

    add proyect

---
+++ monolito/src/main/java/com/groupsApp/monolito/MonolitoApplication.java
@@ -0,0 +1,13 @@
+package com.groupsApp.monolito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonolitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonolitoApplication.class, args);
+	}
+
+}
---
+++ monolito/src/test/java/com/groupsApp/monolito/MonolitoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.groupsApp.monolito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonolitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
